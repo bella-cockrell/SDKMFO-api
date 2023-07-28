aee3a29855a010e87c33a253095d8dc516527b63
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-07-28 15:51:07 +0200

    add domain object Item and PUT endpoint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
-
 from fastapi import FastAPI
+from Domain.Item import Item
 
 app = FastAPI()
 
@@ -13,3 +13,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
